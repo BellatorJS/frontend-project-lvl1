3576aef656b6edfbf2db2c46f11418e49045198d
Aleksey <bellatorjs1@gmail.com>
2022-01-21 18:55:53 +0300

    Solved issue #2

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
-export default getUserName;
+export default greeting;
