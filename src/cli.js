bac558366cb1cf2292b5d5c6a5c115e91b86a9fa
Aleksey <bellatorjs1@gmail.com>
2022-01-24 21:03:24 +0300

    Fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
